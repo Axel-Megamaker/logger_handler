4b48d5ce3907d9adde0f25244df6f022d32c9cb0
Bronken4 <axel.svensson@megamaker.se>
2025-03-03 08:46:25 +0100

    added upload_file function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import requests
 
 def create_folder(path:str, folder:str):
 
@@ -10,8 +11,6 @@ def create_folder(path:str, folder:str):
             print(f"Removing folder: {f}") 
             shutil.rmtree(folder_path)
 
-    
-    
     loggerFiles = f"{path}/{folder}"
     os.makedirs(loggerFiles)
 
@@ -22,9 +21,9 @@ def create_file(path:str, filesNumber:int):
         if f.startswith("logs_"):
             folder_list.append(f)
         else:
-            destination.append(f)
+            destination = f
         
-    destinationPath = f"{path}/{destination[0]}"
+    destinationPath = f"{path}/{destination}"
     if len(folder_list) != 0:
         for f in folder_list:
             files = os.listdir(f"{path}/{f}")
@@ -33,20 +32,31 @@ def create_file(path:str, filesNumber:int):
     else:
         print("No folders found")
 
-    shutil.make_archive(f'{path}/zip/{destination[0]}', 'zip', destinationPath)
-
+    shutil.make_archive(f'{path}/zip/{destination}', 'zip', destinationPath)
 
 def create_zip(path:str, folder:str, filesNumber:int):
     create_folder(path, folder)
     create_file(path, filesNumber)
-        
+
+def create_logger_folder(path:str, name:str):
+    os.makedirs(f"{path}/{f'logs_{name}'}")
+
+#The functions is not ready 
+def upload_file(upload_url:str, zip_file:str):
+    with open(zip_file, 'rb') as file:
+        files = {"file": (zip_file, file, "application/zip")}
+        response = requests.post(upload_url, files=files)
+    
+    print(response.json())
 #__________________________________________________________
 
 #Todays date, that can be used to name the folder
 
-today = '2025-01-14'
+today = '2025-01-16'
 #Add folder name
 folder = f'{today}-logs'
 
 # Create zip takes the path, the folder name and the number of files to be Ziped (path:str, folder:str, filesNumber:int)
-create_zip('logger_files', folder, 2)
+
+create_zip('logger_files', folder, 3)
+
