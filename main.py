1c1b171d43e25401de26b18afb06586fd1e77edc
Bronken4 <axel.svensson@megamaker.se>
2025-03-03 19:08:03 +0100

    updated create_zip func

--- main.py
+++ main.py
@@ -1,62 +1,85 @@
 import os
 import shutil
-import requests
+import logging
+from datetime import datetime
 
-def create_folder(path:str, folder:str):
+#___________LOGGER_____________________________________________________________________________________________________________________________________________
+logger_folder = os.path.join('logger_files', 'logs_LoggerHandler')
+file_ini = 'LH'
+today = datetime.now().strftime("%Y-%m-%d")
+logging.basicConfig(filename=os.path.join(logger_folder, f'{file_ini}_{today}'), level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+#___________________________________________________________________________________________________________________________________________________________
 
+#============Functions==================================
+def create_zip(path:str, folder:str, filesNumber:int):
+    #==================Create folder======================================================================
     for f in os.listdir(path):
         folder_path = os.path.join(path, f)
-        
+
         if os.path.isdir(folder_path) and not f.startswith("logs_"):
-            print(f"Removing folder: {f}") 
+            logger.info(f'Removed folder: {f}') 
             shutil.rmtree(folder_path)
 
-    loggerFiles = f"{path}/{folder}"
+    loggerFiles = os.path.join(path,folder)
     os.makedirs(loggerFiles)
-
-def create_file(path:str, filesNumber:int):
+    logger.info(f'Added folder: {folder}') 
+    
+    #==================Create file==============================================================================
     folder_list = []
     destination = []
     for f in os.listdir(path):
         if f.startswith("logs_"):
             folder_list.append(f)
+            logger.info(f'Added: {f} to list of log folders') 
         else:
             destination = f
+            logger.info(f'File to be zipped: {destination}') 
         
-    destinationPath = f"{path}/{destination}"
+    destinationPath = os.path.join(path,destination)
     if len(folder_list) != 0:
-        for f in folder_list:
-            files = os.listdir(f"{path}/{f}")
+        logger.info(f'Log folders: {len(folder_list)}') 
+        for folder in folder_list:
+            files = os.listdir(os.path.join(path, folder))
             for file in files[-filesNumber:]:
-                shutil.copyfile(f"{path}/{f}/{file}", f"{destinationPath}/{file}")            
+                shutil.copyfile(os.path.join(path, folder, file), os.path.join(destinationPath, file))
+                logger.info(f'Copying: {file} TO FOLDER {destinationPath}')             
     else:
-        print("No folders found")
+        logger.info(f'No folders found') 
 
-    shutil.make_archive(f'{path}/zip/{destination}', 'zip', destinationPath)
+    logger.info(f'Created zipfile: {destination}.zip IN FOLDER {path}\zip')
+    shutil.make_archive(os.path.join(path, "zip", destination), "zip", destinationPath)
+    #=======================================================================================================
 
-def create_zip(path:str, folder:str, filesNumber:int):
-    create_folder(path, folder)
-    create_file(path, filesNumber)
+def remove_files(path:str, numberOfDays:int):
+    folder_list = []
+    file_list = []
+    for f in os.listdir(path):
+        if f.startswith("logs_"):
+            folder_list.append(f)
 
-def create_logger_folder(path:str, name:str):
-    os.makedirs(f"{path}/{f'logs_{name}'}")
+    if len(folder_list) != 0:
+        for folder in folder_list:
+            files = os.listdir(os.path.join(path, folder))
+            for file in files:
+                file_list.append(file)
+            if len(file_list) >= numberOfDays:
+                print(f'{folder} : ({len(file_list)} file[s])')
 
-#The functions is not ready 
-def upload_file(upload_url:str, zip_file:str):
-    with open(zip_file, 'rb') as file:
-        files = {"file": (zip_file, file, "application/zip")}
-        response = requests.post(upload_url, files=files)
-    
-    print(response.json())
-#__________________________________________________________
+                n = len(file_list) - numberOfDays
+                if n >= 0:
+                    print(file_list[:n])
+                    for f in file_list[:n]:#
+                        os.remove(os.path.join(path, folder, f))
+            else:
+                print(f'{folder} : {len(file_list)} file[s]')
+            file_list.clear()
 
-#Todays date, that can be used to name the folder
+#===================Main==================================
+today = datetime.now().strftime("%Y-%m-%d")
 
-today = '2025-01-16'
-#Add folder name
+path = 'logger_files'
 folder = f'{today}-logs'
+filesNumber = 3
 
-# Create zip takes the path, the folder name and the number of files to be Ziped (path:str, folder:str, filesNumber:int)
-
-create_zip('logger_files', folder, 3)
-
+create_zip(path, folder, filesNumber)
